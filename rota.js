c9c7f4dc5e2c29434707987b48363bc442946bf0
Guilherme Vitor <109116903+zeppelito@users.noreply.github.com>
2023-01-03 23:35:09 -0300

    Delete rota.js

--- rota.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express();
-
-app.get('/httpbin.org', (request, response) => {
-    return response.send("O usúario está devidamente 100% conectado");
-});
-
-app.listen(777);
-console.log("Rodando...")
